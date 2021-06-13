e7365a555763a95c90b30243f2dc3043323f4adc
zahraariff <fatimazahra0901@gmail.com>
2021-06-13 11:21:38 +0800

    Tour with display methods

--- Tour.java
+++ Tour.java
@@ -5,9 +5,13 @@
  */
 package alwaysontime;
 
+import java.util.Stack;
+
 public class Tour{
     MyLinkedList<RouteGraph<Customer, Double>> tourRoutes; //Linked list of routes in a tour
     public static MyLinkedList<Customer> allCustomer = new MyLinkedList<>(); // Linked List for all customer
+    Stack<Customer> stack = new Stack<>();
+    Stack<Customer> visited = new Stack<>();
     public static Customer depot; //A static depot variable(the depot is the same for all routes/tours
     private double tourCost; //Each tour has a cost (Sum of all route cost)
     private int vehicleID; //To identify each vehicle in a tour
@@ -29,7 +33,7 @@ public class Tour{
     
     
         //create a tour route list when tour object is created
-    public void newRoute(){
+    public void newRouteMCTS(){
         //Increment vehicleID everytime a new vehicle is used for a new route
         vehicleID++;
         //Declare a new route and assign a new vehicle as a parameter
@@ -38,7 +42,16 @@ public class Tour{
         newRoute.addVertex(depot);
         //add new route to the tourRoutes list
         tourRoutes.add(newRoute);
+        //stack.push(depot);
     }
+    
+//    public void newRouteDFS(){
+//	vehicleID++;
+//	//Declare a new route and assign a new vehicle as a parameter
+//	RouteGraph<Customer,Double> newRoute= new RouteGraph<>(new Vehicle(vehicleCapacity, vehicleID));
+//	//add the depot into the route as the first stop
+//	//stack.push(depot); // tour.stack.currentRoute.push
+//    }
     
     //Constructor to set tour with a complete route list
     public Tour(MyLinkedList<RouteGraph<Customer,Double>> tourRoutes) {
@@ -101,10 +114,40 @@ public class Tour{
         System.out.println("\nMCTS Simulation\nTour\nTour Cost: "+getTourCost());
         for(int i=0;i<tourRoutes.getSize();i++){
             System.out.println("Vehicle "+tourRoutes.get(i).vehicle.getVehicleID());
-            System.out.println(tourRoutes.get(i).printRoute());
+            if(tourRoutes.get(i).getSize() != 2){
+                System.out.println(tourRoutes.get(i).printRoute());
+            }
             System.out.println("Capacity: "+tourRoutes.get(i).vehicle.getSize());
         }
-        System.out.println("** Time elapsed: " + m.getElapsed() + "seconds **");
+        System.out.println("** Time elapsed: " + m.getElapsed() + " seconds **");
+    }
+    
+    public void displayDFS(DFS dfs) {
+	System.out.println("\nBasic Simulation \nTour\nTour Cost: "+getTourCost());
+            for(int i = 0; i<tourRoutes.getSize(); i++) {
+                //Only prints out non-empty routes
+                //Empty routes have two stops which is 0->0
+                if(tourRoutes.get(i).getSize() > 2){
+                System.out.println();
+		System.out.println("Vehicle "+tourRoutes.get(i).vehicle.getVehicleID());
+                    System.out.println(tourRoutes.get(i).printRoute());
+                    System.out.println("Capacity: "+tourRoutes.get(i).vehicle.getSize());
+                    System.out.println("Cost: "+tourRoutes.get(i).getRouteCost());
+                }
+            }
+	System.out.println("** Time elapsed: " + dfs.getElapsed() + "seconds **\n");
+    }
+	    
+    public void displayGreedy(Greedy g) {
+        System.out.println("Greedy \nTour\nTour Cost: "+getTourCost());
+	for(int i = 0; i<tourRoutes.getSize(); i++) {
+            System.out.println();
+            System.out.println("Vehicle "+tourRoutes.get(i).vehicle.getVehicleID());
+            System.out.println(tourRoutes.get(i).printRoute());
+            System.out.println("Capacity: "+tourRoutes.get(i).vehicle.getSize());
+            System.out.println("Cost: "+tourRoutes.get(i).getRouteCost());
+	}
+        System.out.println("** Time elapsed: " + g.getElapsed() + "seconds **\n");
     }
      
 }
